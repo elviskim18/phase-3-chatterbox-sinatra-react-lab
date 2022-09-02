e8b3ed7fa94070025e892e0c61d91b36277a93df
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-02 14:14:25 +0300

    commit tree b94ca460117173d151f7011a5af712f261208308

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,33 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    messages = Message.all.order(created_at: :asc)
+    messages.to_json
+  end
+
+  post '/messages' do
+    messagenew = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    messagenew.to_json
+  end
+
+  patch '/messages/:id' do
+    messageupdate = Message.find(params[:id])
+    messageupdate.update(
+      body: params[:body],
+      username: params[:username]
+    )
+    messageupdate.to_json
+  
+  end
+
+  delete '/messages/:id' do
+    delmessage = Message.find(params[:id])
+    delmessage.destroy
+    delmessage.to_json
+  end
   
 end
